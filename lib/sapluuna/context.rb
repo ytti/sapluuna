70f061c31f0a7e52cc84cab04ba866b39a44c0c3
Saku Ytti <saku@ytti.fi>
2015-02-07 00:27:06 +0200

    add automatic indent
    Now inherited files can be written with indent 0, as indent is
    discovered from line where inherited config is added.
    
    so
    
       <% import 'foo' %>
    
    would add 3 spaces indent to every line in 'foo', except first line, as
    that is already in by the time we read 'foo'.

--- lib/sapluuna/context.rb
+++ lib/sapluuna/context.rb
@@ -49,13 +49,20 @@ class Sapluuna
       sapl     = Sapluuna.new @opts
       output   = sapl.parse template
       @discovered_variables += sapl.discovered_variables
-      output
+      add_indent output.lines, @output.lines.last
     end
 
     def resolve_file file
       # should we avoid ../../../etc/passwd style input here?
       # why? we control templates?
       File.join @root_directory, file
+    end
+
+    def add_indent output, indent_hint
+      indent_size = indent_hint.match(/\A\s*/)[0].size
+      first_line  = output[0]
+      output = output[1..-1].map { |line| ' ' * indent_size + line } if output.size > 1
+      output.unshift(first_line).join
     end
 
     def method_missing method, *args
